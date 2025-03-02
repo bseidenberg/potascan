1ca8ebe84fec51c41ada2a5e7ed109a519a962cc
Benjamin E. Seidenberg <astronut@dlgeek.net>
2025-03-02 09:17:33 -0800

    License under GPLv3

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ POTAScan is a utility to download spots from POTA and automatically tune a radio
 
 -- Benjamin, WY2K
 
+Copyright (C) 2023-2025 Benjamin Seidenberg
 
-Copyright 2023-2024 Benjamin Seidenberg
-All Rights Reserved
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-(For now - I plan to open source this soon.)
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import wx, wx.lib.scrolledpanel, wx.lib.intctrl
